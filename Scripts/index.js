57d45ad2810db87084aed7af3fc77c64aef485ff
SandhanuDulmeth <sandhanu12@gmail.com>
2024-12-13 13:49:15 +0530

    Commit

---
+++ Scripts/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    DESCMyProject();
+});
+
+function DESCMyProject() {
+    Swal.fire({
+        position: "center",
+        icon: "info",
+        title: "Welcome to my Spring Boot project",
+        showConfirmButton: false,
+        timer: 1500
+      });
+      
+}
