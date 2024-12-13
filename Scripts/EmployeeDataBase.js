b54ac2dbdb0ac6a72c2146f16f4cc51a71a63dc3
SandhanuDulmeth <sandhanu12@gmail.com>
2024-12-13 16:32:25 +0530

    Commit

--- Scripts/EmployeeDataBase.js
+++ Scripts/EmployeeDataBase.js
@@ -6,27 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
     updateTable();
 });
 
-// function updateTable(){
-//     let tbody=document.getElementById("tbody");
-//     let body="";
-//     fetch("http://localhost:8080/")
-//   .then(res => res.json())
-//   .then(data=>{data.forEach(element => {
-//     body+=`<tr>
-//     <td>${element.id}</td>
-//     <td>${element.name}</td>
-//     <td>${element.email}</td>
-//     <td>${element.address}</td>
-//     <td>
-//         <button class="btn btn-sm btn-primary" onclick="editEmployee(${element.id}, ${element.name}, ${element.email}, ${element.address})">Edit</button>
-//         <button class="btn btn-sm btn-danger">Delete</button>
-//     </td>
-// </tr>`
-//   });
-// tbody.innerHTML=body;
-// })
-
-// }
 
 function updateTable() {
     let tbody = document.getElementById("tbody");
@@ -42,21 +21,35 @@ function updateTable() {
                 <td>${element.address}</td>
                 <td>
                     <button class="btn btn-sm btn-primary" onclick="editEmployee(${element.id}, '${element.name}', '${element.email}', '${element.address}')">Edit</button>
-                    <button class="btn btn-sm btn-danger" onclick="deleteEmployee()">Delete</button>
+                    <button class="btn btn-sm btn-danger" onclick="deleteEmployee(${element.id})">Delete</button>
                 </td>
             </tr>`;
             });
             tbody.innerHTML = body;
         });
 }
-function deleteEmployee(){
+function deleteEmployee(id) {
+    const raw = "";
 
+    const requestOptions = {
+      method: "DELETE",
+      body: raw,
+      redirect: "follow"
+    };
     
+   
+    fetch(`http://localhost:8080/${id}`, requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        console.log(result)
+        updateTable();
+    })
+      .catch((error) => console.error(error));
 }
 
 
 function editEmployee(id, name, email, address) {
-    // Populate the form fields with the selected employee's data
+    
     document.getElementById('employeeId').value = id;
     document.getElementById('employeeName').value = name;
     document.getElementById('employeeEmail').value = email;
@@ -94,7 +87,7 @@ function editDataBaseFunction() {
         .catch((error) => console.error(error));
 
     alert(`Employee ${id} updated with Name: ${name}, Email: ${email}, and Address: ${address}`);
-    // Here you can add functionality to update the employee list dynamically or send data to a server
+  
 
 }
 
