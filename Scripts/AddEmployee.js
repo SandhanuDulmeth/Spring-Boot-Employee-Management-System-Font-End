37c5974cf37aca97d87007c6c0fff1e17e51d38a
SandhanuDulmeth <sandhanu12@gmail.com>
2024-12-13 15:26:53 +0530

    Commit

--- Scripts/AddEmployee.js
+++ Scripts/AddEmployee.js
@@ -3,26 +3,27 @@ function addEmployeeFunction(){
 let name=document.getElementById("name").value;
 let email=document.getElementById("email").value;
 let address=document.getElementById("address").value;
+console.log(name,address);
 
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    const raw = JSON.stringify({
-      "name": name,
-      "address": address
-    });
-    
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow"
-    };
-    
-    fetch("http://localhost:8080/", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
 
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "name": name,
+  "address": address
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("http://localhost:8080/", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
 
 }
